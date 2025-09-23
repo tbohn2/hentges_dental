bea4c675321a986fe1248b8505bb571245568fa1
Tanner Bohn <tanner.bohn@truneighbor.co>
2025-09-23 14:53:59 -0700

    Specifed root

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+    turbopack: {
+        root: __dirname,
+    },
+};
 
 export default nextConfig;
