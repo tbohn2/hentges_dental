dc8ba0e1b6a578dda5ef7d9cdcec1e942c929479
Tanner Bohn <tanner.bohn@truneighbor.co>
2025-10-10 08:03:29 -0700

    Added new patient paperwork downloads

--- app/new-patient-forms/page.js
+++ app/new-patient-forms/page.js
@@ -18,8 +18,15 @@ If you have questions about any of the paperwork, please give us a call at (480)
 
 const content = [
     { heading: '', text: ["Access our new patient forms online to save time at your dental appointment. Simply download, print, and fill out the forms before your visit to Hentges Dental in Mesa, AZ. Bring the completed paperwork with you to help us streamline your check-in process."] },
-    { heading: '', text: [<>If you have any questions about our dental forms or need assistance, please contact our office at <a href="tel:(480) 964-2131" className="text-secondary hover:underline">(480) 964-2131</a>. Our friendly dental team is here to help!</>] },
-    { heading: '', text: [<a href='' className="text-tertiary hover:underline">Download New Patient Forms</a>] }
+    { heading: '', text: [<>If you have any questions about our dental forms or need assistance, please contact our office at <a href="tel:(480) 964-2131" className="text-tertiary hover:underline">(480) 964-2131</a>. Our friendly dental team is here to help!</>] },
+    {
+        heading: 'Download New Patient Forms', text: [
+            <a href="/documents/Patient_Registration.pdf" download className="text-tertiary hover:underline">Patient Registration (PDF)</a>,
+            <a href="/documents/Medical_History.pdf" download className="text-tertiary hover:underline">Medical History (PDF)</a>,
+            <a href="/documents/Welcome_Financial.pdf" download className="text-tertiary hover:underline">Welcome & Financial Policy (PDF)</a>,
+            <a href="/documents/HIPAA.pdf" download className="text-tertiary hover:underline">HIPAA Acknowledgment (PDF)</a>
+        ]
+    }
 ]
 
 export default function NewPatientForms() {
