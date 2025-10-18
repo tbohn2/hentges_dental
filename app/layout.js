38f923c3be557221e924acf19e07d7184329e624
Tanner Bohn <tanner.bohn@truneighbor.co>
2025-10-18 14:20:12 -0700

    Added fade-in animation on first load

--- app/layout.js
+++ app/layout.js
@@ -118,7 +118,7 @@ export default function RootLayout({ children }) {
             ))}
           </div>
           <Header />
-          <div className="w-full flex flex-col items-center justify-center my-4">
+          <div className="w-full flex flex-col items-center justify-center my-4 page-fade-in">
             {/* <PageTransition> */}
             {children}
             {/* </PageTransition> */}
